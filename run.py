06ada6a9201311ff3172296a880ea50cd2de46ae
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2023-01-24 15:32:17 +0530

    update run.py

--- run.py
+++ run.py
@@ -23,22 +23,23 @@ for im_basename in ('input_0', 'inputSimplest', 'output'):
     if 'input_0' == im_basename:
         # the scale is based on then input image, first to be computed
         scale_h = 127. / max(h)
-        # define RGB colors and dark variants
-        color = {'R' : (255, 0, 0), 'G' : (0, 255, 0), 'B' : (0, 0, 255)}
-        dark = {'R' : (63, 0, 0), 'G' : (0, 63, 0), 'B' : (0, 0, 63)}
-        # draw histograms of R, G, B values
-        for channel in ('R', 'G', 'B'):
-            # PIL stores the RGB histograms in a single array
-            xoffset = {'R' : 0, 'G' : 256, 'B' : 512}[channel]
-            # new histogram image
-            him = PIL.Image.new('RGB', (256, 128), (255, 255, 255))
-            draw = PIL.ImageDraw.Draw(him)
-            for x in range(0, 256):
-                hx = int(h[x+xoffset] * scale_h)
-                if hx <= 127:
-                    draw.line([(x, 127), (x, 127 - hx)],
-                                  fill=color[channel])
-                else:
-                    draw.line([(x, 127), (x, 0)],
-                                  fill=dark[channel])
-            him.save(im_basename + '_%s.png' % channel)
+        
+    # define RGB colors and dark variants
+    color = {'R' : (255, 0, 0), 'G' : (0, 255, 0), 'B' : (0, 0, 255)}
+    dark = {'R' : (63, 0, 0), 'G' : (0, 63, 0), 'B' : (0, 0, 63)}
+    # draw histograms of R, G, B values
+    for channel in ('R', 'G', 'B'):
+        # PIL stores the RGB histograms in a single array
+        xoffset = {'R' : 0, 'G' : 256, 'B' : 512}[channel]
+        # new histogram image
+        him = PIL.Image.new('RGB', (256, 128), (255, 255, 255))
+        draw = PIL.ImageDraw.Draw(him)
+        for x in range(0, 256):
+            hx = int(h[x+xoffset] * scale_h)
+            if hx <= 127:
+                draw.line([(x, 127), (x, 127 - hx)],
+                                fill=color[channel])
+            else:
+                draw.line([(x, 127), (x, 0)],
+                                fill=dark[channel])
+        him.save(im_basename + '_%s.png' % channel)
